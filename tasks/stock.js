3edea237513dd90bffe7164509a3162504b0f593
Joseph Ting <josephting@users.noreply.github.com>
2021-02-22 01:47:09 +0900

    Add date to message

--- tasks/stock.js
+++ tasks/stock.js
@@ -2,7 +2,7 @@ module.exports = {
     cronTime: '20 */15 * * * *',
     onTick: (client) => {
         client.channels.fetch(process.env.HEARTBEAT_CHANNEL_ID)
-            .then(channel => channel.send('Stock cron'))
+            .then(channel => channel.send(`Stock cron @ ${(new Date()).toLocaleString()}`))
             .catch(console.error)
     }
 }
